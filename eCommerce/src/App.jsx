73dd8bed6e84061ba646238b9a1d534c6926e767
aricansena <senaarican247@gmail.com>
2024-10-01 23:38:19 +0300

    e-commerce update

--- eCommerce/src/App.jsx
+++ eCommerce/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import PageContainer from './container/PageContainer'
 import Header from './components/Header'
+import RouterConfig from './config/RouterConfig'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       <PageContainer>
         <Header />
+        <RouterConfig />
       </PageContainer>
     </div>
   )
