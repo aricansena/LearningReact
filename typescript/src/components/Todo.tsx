7398558f47f3cccbce04f81a3869b1d300721796
aricansena <senaarican247@gmail.com>
2024-10-08 19:40:57 +0300

    todoApp-last

--- typescript/src/components/Todo.tsx
+++ typescript/src/components/Todo.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiCircleRemove } from "react-icons/ci";
-import { CiHeart } from "react-icons/ci";
+import { CiEdit } from "react-icons/ci";
+import { IoCheckmarkDone } from "react-icons/io5";
 
-function Todo() {
+import { TodoType } from '../types/Type';
+import { useDispatch } from 'react-redux';
+import { removeTodoById, updateTodoById } from '../redux/todoSlice';
+
+interface TodoProps {
+    todoProps: TodoType
+}
+
+function Todo({ todoProps }: TodoProps) {
+    const { id, content } = todoProps;
+    const dispatch = useDispatch();
+    const [editable, setEditable] = useState<boolean>(false);
+    const [newtodo, setNewTodo] = useState<string>(content);
+    const handleRemoveTodo = () => {
+        dispatch(removeTodoById(id))
+    }
+    const handleUpdateTodo = () => {
+        const payload: TodoType = {
+            id: id,
+            content: newtodo
+        }
+        dispatch(updateTodoById(payload))
+        setEditable(false)
+    }
     return (
         <div className='todos flex-space-between'>
+            {editable ? <input className='edit-todo-input' type='text'
+                value={newtodo}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)}
+            ></input> : <div>{content}</div>}
             <div>
-                ilk todo
-            </div>
-            <div>
-                <CiCircleRemove className='icon' />
-                <CiHeart className='icon' />
+                <CiCircleRemove onClick={handleRemoveTodo} className='icon' />
+                {editable ? <IoCheckmarkDone onClick={handleUpdateTodo} className='icon' /> : <CiEdit onClick={() => setEditable(!editable)} className='icon' />}
             </div>
         </div>
     )
--- typescript/src/components/TodoList.tsx
+++ typescript/src/components/TodoList.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Todo from './Todo'
+import { useSelector } from 'react-redux'
+import { RootState } from '../redux/store'
+import { TodoType } from '../types/Type'
 
 function TodoList() {
-
+    const { todos } = useSelector((state: RootState) => state.todo)
     return (
         <div>
-
+            {todos && todos.map((todo: TodoType) => (
+                <Todo key={todo.id} todoProps={todo} />
+            ))}
         </div>
     )
 }
